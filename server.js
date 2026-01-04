19389c55258b9e54fd69ca5ea4739b92c657f61f
Krishna Prasad Guntupalli <krishna.gprasad@juspay.in>
2026-01-04 23:23:51 +0530

    Added image path

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Basic Auth (Placeholder)
 const JUSPAY_AUTH = process.env.JUSPAY_AUTH || 'Basic dummy';
 
